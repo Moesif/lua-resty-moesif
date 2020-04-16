156a79b8f098e454fcda094ceba27d8f963c7878
Keyur <keyur.kv@gmail.com>
2020-04-15 22:28:27 -0400

    Refactor: Use LRU Cache for User Id
    Refactor: Use LRU Cache for User Id
    Refactor: Remove extra checks for compressed body
    Bump version to 1.1.8

--- lua/resty/moesif/moesif_global.lua
+++ lua/resty/moesif/moesif_global.lua
@@ -1,3 +1,6 @@
-scale_config_response_dict = {}
-scale_user_id_dict = {}
-scale_last_updated_time_dict = {}
+local lrucache = require "resty.lrucache"
+
+user_id_cache, uic_err = lrucache.new(10000)  -- allow up to 10000 items in the cache
+if not user_id_cache then
+    ngx.log(ngx.CRIT, "failed to create the cache: " .. (uic_err or "unknown"))
+end
