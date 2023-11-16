20f12350e9e566233bfb28fb54f8f8f36cdbdb92
Keyur <keyur.kv@gmail.com>
2023-11-15 18:38:36 -0800

    Refactor: Add try/catch when fetching request/response body
    Refactor: Add try/catch when fetching request/response body

--- lua/resty/moesif/read_req_body.lua
+++ lua/resty/moesif/read_req_body.lua
@@ -3,10 +3,12 @@ local _M = {}
 function _M.read_request_body()
     local req_body = ""
     local req_post_args = {}
-    ngx.req.read_body()
-    req_body = ngx.req.get_body_data()
-    -- keep in memory the bodies for this request
-    ngx.var.moesif_req_body = req_body
+    local success, err = pcall(function()
+        ngx.req.read_body()
+        req_body = ngx.req.get_body_data()
+        -- keep in memory the bodies for this request
+        ngx.var.moesif_req_body = req_body
+    end)
 end
 
 _M.read_request_body()
--- lua/resty/moesif/read_res_body.lua
+++ lua/resty/moesif/read_res_body.lua
@@ -1,10 +1,16 @@
 local _M = {}
  
 function _M.read_response_body()
-    local chunk = ngx.arg[1]
-    ngx.ctx.buffered = (ngx.ctx.buffered or "") .. chunk
-    if ngx.arg[2] then
-        ngx.var.moesif_res_body = ngx.ctx.buffered
+    local status, err = pcall(function()
+        local chunk = ngx.arg[1]
+        ngx.ctx.buffered = (ngx.ctx.buffered or "") .. chunk
+        if ngx.arg[2] then
+            ngx.var.moesif_res_body = ngx.ctx.buffered
+        end
+    end)
+
+    if not status then
+        ngx.var.moesif_res_body = nil
     end
 end
 
