4729a011bd6129368b606c8327563c39498a0de9
Keyur <keyur.kv@gmail.com>
2024-11-30 14:32:08 -0800

    Refactor: Working with gov rules
    Refactor: Working with gov rules

--- lua/resty/moesif/read_req_body.lua
+++ lua/resty/moesif/read_req_body.lua
@@ -1,7 +1,43 @@
 local _M = {}
+
+local moesif_client = require "moesif.common.moesif_client"
+local socket = require "socket"
  
+local function dump(o)
+    if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+    else
+      return tostring(o)
+    end
+  end
+
+-- local config = ngx.shared.moesif_conf
+-- config = moesif_client.set_default_config_value(config)
+local config = moesif_client.set_default_config_value(ngx.shared.moesif_conf)
+
+local req_get_method = ngx.req.get_method()
+local req_get_headers = ngx.req.get_headers()
+
+ngx.log(ngx.DEBUG, "[moesif] req_get_methody: ", dump(req_get_method))
+ngx.log(ngx.DEBUG, "[moesif] req_get_headers: ", dump(req_get_headers))
+ngx.log(ngx.DEBUG, "[moesif] config: ", dump(config))
+
+for _, key in ipairs({"application_id", "debug"}) do
+    local value = config:get(key)
+    ngx.log(ngx.ERR, "Key: ", key, ", Value: ", dump(value) or "nil")
+end
+
 function _M.read_request_body()
-    local req_body = ""
+    local start_access_phase_time = socket.gettime()*1000
+    
+    -- local req_body = ""
+    local req_body, res_body = "", ""
+    -- TODO: Figure out
     local req_post_args = {}
     local success, err = pcall(function()
         ngx.req.read_body()
@@ -9,6 +45,14 @@ function _M.read_request_body()
         -- keep in memory the bodies for this request
         ngx.var.moesif_req_body = req_body
     end)
+
+    ngx.ctx.moesif = {
+        req_body = req_body,
+        res_body = res_body,
+        req_post_args = req_post_args
+      }
+
+    moesif_client.govern_request(config, start_access_phase_time, req_get_method, req_get_headers)
 end
 
 _M.read_request_body()
--- lua/resty/moesif/send_event.lua
+++ lua/resty/moesif/send_event.lua
@@ -3,6 +3,7 @@ local url = require "socket.url"
 local HTTPS = "https"
 local moesif_ser = require "moesif_ser"
 local log = require "log"
+local moesif_client = require "moesif.common.moesif_client"
 
 local function isempty(s)
   return s == nil or s == ''
@@ -11,101 +12,108 @@ end
 -- Global config
 local config = ngx.shared.moesif_conf;
 
+config = moesif_client.set_default_config_value(config)
+
 -- Set Default values.
-if isempty(config:get("disable_transaction_id")) then
-  config:set("disable_transaction_id", false)
-end
+-- if isempty(config:get("disable_transaction_id")) then
+--   config:set("disable_transaction_id", false)
+-- end
 
-if isempty(config:get("api_endpoint")) then
-  config:set("api_endpoint", "https://api.moesif.net")
-end
+-- if isempty(config:get("api_endpoint")) then
+--   config:set("api_endpoint", "https://api.moesif.net")
+-- end
 
-if isempty(config:get("timeout")) then
-  config:set("timeout", 1000)
-end
+-- if isempty(config:get("timeout")) then
+--   config:set("timeout", 1000)
+-- end
 
-if isempty(config:get("connect_timeout")) then
-  config:set("connect_timeout", 1000)
-end
+-- if isempty(config:get("connect_timeout")) then
+--   config:set("connect_timeout", 1000)
+-- end
 
-if isempty(config:get("send_timeout")) then
-  config:set("send_timeout", 2000)
-end
+-- if isempty(config:get("send_timeout")) then
+--   config:set("send_timeout", 2000)
+-- end
 
-if isempty(config:get("keepalive")) then
-  config:set("keepalive", 5000)
-end
+-- if isempty(config:get("keepalive")) then
+--   config:set("keepalive", 5000)
+-- end
 
-if isempty(config:get("disable_capture_request_body")) then
-  config:set("disable_capture_request_body", false)
-end
+-- if isempty(config:get("disable_capture_request_body")) then
+--   config:set("disable_capture_request_body", false)
+-- end
 
-if isempty(config:get("disable_capture_response_body")) then
-  config:set("disable_capture_response_body", false)
-end
+-- if isempty(config:get("disable_capture_response_body")) then
+--   config:set("disable_capture_response_body", false)
+-- end
 
-if isempty(config:get("request_masks")) then
-  config:set("request_masks", "")
-end
+-- if isempty(config:get("request_masks")) then
+--   config:set("request_masks", "")
+-- end
 
-if isempty(config:get("request_body_masks")) then
-  config:set("request_body_masks", "")
-end
+-- if isempty(config:get("request_body_masks")) then
+--   config:set("request_body_masks", "")
+-- end
 
-if isempty(config:get("request_header_masks")) then
-  config:set("request_header_masks", "")
-end
+-- if isempty(config:get("request_header_masks")) then
+--   config:set("request_header_masks", "")
+-- end
 
-if isempty(config:get("response_masks")) then
-  config:set("response_masks", "")
-end
+-- if isempty(config:get("response_masks")) then
+--   config:set("response_masks", "")
+-- end
 
-if isempty(config:get("response_body_masks")) then
-  config:set("response_body_masks", "")
-end
+-- if isempty(config:get("response_body_masks")) then
+--   config:set("response_body_masks", "")
+-- end
 
-if isempty(config:get("response_header_masks")) then
-  config:set("response_header_masks", "")
-end
+-- if isempty(config:get("response_header_masks")) then
+--   config:set("response_header_masks", "")
+-- end
 
-if isempty(config:get("batch_size")) then
-  config:set("batch_size", 200)
-end
+-- if isempty(config:get("batch_size")) then
+--   config:set("batch_size", 200)
+-- end
 
-if isempty(config:get("debug")) then
-  config:set("debug", false)
-end
+-- if isempty(config:get("debug")) then
+--   config:set("debug", false)
+-- end
 
-if isempty(config:get("batch_max_time")) then
-  config:set("batch_max_time", 2)
-elseif config:get("batch_max_time") > 30 then 
-  ngx.log(ngx.ERR, "[moesif] Resetting Batch max time config value (" .. tostring(config:get("batch_max_time")) .. ") to max allowed (30 seconds)");
-  config:set("batch_max_time", 30)
-end
+-- if isempty(config:get("batch_max_time")) then
+--   config:set("batch_max_time", 2)
+-- elseif config:get("batch_max_time") > 30 then 
+--   ngx.log(ngx.ERR, "[moesif] Resetting Batch max time config value (" .. tostring(config:get("batch_max_time")) .. ") to max allowed (30 seconds)");
+--   config:set("batch_max_time", 30)
+-- end
 
-if isempty(config:get("max_callback_time_spent")) then
-  config:set("max_callback_time_spent", 2000)
-end
+-- if isempty(config:get("max_callback_time_spent")) then
+--   config:set("max_callback_time_spent", 2000)
+-- end
 
-if isempty(config:get("disable_gzip_payload_decompression")) then
-  config:set("disable_gzip_payload_decompression", false)
-end
+-- if isempty(config:get("disable_gzip_payload_decompression")) then
+--   config:set("disable_gzip_payload_decompression", false)
+-- end
 
-if isempty(config:get("queue_scheduled_time")) then
-  config:set("queue_scheduled_time", os.time{year=1970, month=1, day=1, hour=0})
-end
+-- if isempty(config:get("queue_scheduled_time")) then
+--   config:set("queue_scheduled_time", os.time{year=1970, month=1, day=1, hour=0})
+-- end
 
-if isempty(config:get("max_body_size_limit")) then
-  config:set("max_body_size_limit", 100000)
-end
+-- if isempty(config:get("max_body_size_limit")) then
+--   config:set("max_body_size_limit", 100000)
+-- end
 
-if isempty(config:get("authorization_header_name")) then
-  config:set("authorization_header_name", "authorization")
-end
+-- if isempty(config:get("authorization_header_name")) then
+--   config:set("authorization_header_name", "authorization")
+-- end
 
-if isempty(config:get("authorization_user_id_field")) then
-  config:set("authorization_user_id_field", "sub")
-end
+-- if isempty(config:get("authorization_user_id_field")) then
+--   config:set("authorization_user_id_field", "sub")
+-- end
+
+-- -- TODO: Figure out - request_query_masks = {default = {}, type = "array", elements = typedefs.header_name}
+-- if isempty(config:get("request_query_masks")) then
+--   config:set("request_query_masks", {})
+-- end
 
 -- User Agent String
 local user_agent_string = "lua-resty-moesif/1.3.12"
