edf55be759c56735d8aaea3b203ec4bf43efbf13
Keyur <keyur.kv@gmail.com>
2024-12-01 15:20:55 -0800

    Refactor: Add serialization helper
    Refactor: Add serialization helper

--- lua/resty/moesif/moesif_ser.lua
+++ lua/resty/moesif/moesif_ser.lua
@@ -10,6 +10,7 @@ local base64 = require "moesifapi.lua.base64"
 local helpers = require "helpers"
 local _M = {}
 local ngx_log = ngx.log
+local ser_helper = require "moesifapi.lua.serializaiton_helper"
 
 local function dump(o)
   if type(o) == 'table' then
@@ -35,115 +36,6 @@ local function split(str, character)
   end
 
   return result
-end
-
--- Mask Body
-local function mask_body(body, masks)
-  if masks == nil then return body end
-  if body == nil then return body end
-  for mask_key, mask_value in pairs(masks) do
-    mask_value = mask_value:gsub("%s+", "")
-    if body[mask_value] ~= nil then body[mask_value] = nil end
-    for body_key, body_value in next, body do
-        if type(body_value)=="table" then mask_body(body_value, masks) end
-    end
-  end
-  return body
-end
-
-local function base64_encode_body(body)
-  return base64.encode(body), 'base64'
-end
-
-local function is_valid_json(body)
-    return type(body) == "string" 
-        and string.sub(body, 1, 1) == "{" or string.sub(body, 1, 1) == "["
-end
-
-local function process_data(body, mask_fields)
-  local body_entity = nil
-  local body_transfer_encoding = nil
-  local is_deserialised, deserialised_body = pcall(cjson_safe.decode, body)
-  if not is_deserialised  then
-      body_entity, body_transfer_encoding = base64_encode_body(body)
-  else
-      if next(mask_fields) == nil then
-          body_entity, body_transfer_encoding = deserialised_body, 'json' 
-      else
-          local ok, mask_result = pcall(mask_body, deserialised_body, mask_fields)
-          if not ok then
-            body_entity, body_transfer_encoding = deserialised_body, 'json' 
-          else
-            body_entity, body_transfer_encoding = mask_result, 'json' 
-          end
-      end
-  end
-  return body_entity, body_transfer_encoding
-end
-
-local function decompress_body(body, masks)
-  local body_entity = nil
-  local body_transfer_encoding = nil
-
-  local ok, decompressed_body = pcall(zzlib.gunzip, body)
-  if not ok then
-    if debug then
-      ngx.log(ngx.DEBUG, "[moesif] failed to decompress body: ", decompressed_body)
-    end
-    body_entity, body_transfer_encoding = base64_encode_body(body)
-  else
-    if debug then
-      ngx.log(ngx.DEBUG, " [moesif]  ", "successfully decompressed body: ")
-    end
-    if is_valid_json(decompressed_body) then 
-        body_entity, body_transfer_encoding = process_data(decompressed_body, masks)
-    else 
-        body_entity, body_transfer_encoding = base64_encode_body(decompressed_body)
-    end
-  end
-  return body_entity, body_transfer_encoding
-end
-
-local function mask_headers(headers, mask_fields)
-  local mask_headers = nil
-
-  for k,v in pairs(mask_fields) do
-    mask_fields[k] = v:lower()
-  end
-
-  local ok, mask_result = pcall(mask_body, headers, mask_fields)
-  if not ok then
-    mask_headers = headers
-  else
-    mask_headers = mask_result
-  end
-  return mask_headers
-end
-
-local function mask_body_fields(body_masks_config, deprecated_body_masks_config)
-  if next(body_masks_config) == nil then
-    return deprecated_body_masks_config
-  else
-    return body_masks_config
-  end
-end
-
-local function parse_body(headers, body, mask_fields, config)
-  local body_entity = nil
-  local body_transfer_encoding = nil
-
-  if headers["content-type"] ~= nil and is_valid_json(body) then 
-    body_entity, body_transfer_encoding = process_data(body, mask_fields)
-  elseif headers["content-encoding"] ~= nil and type(body) == "string" and string.find(headers["content-encoding"], "gzip") then
-    if not config:get("disable_gzip_payload_decompression") then 
-      body_entity, body_transfer_encoding = decompress_body(body, mask_fields)
-    else
-      body_entity, body_transfer_encoding = base64_encode_body(body)
-    end
-  else
-    body_entity, body_transfer_encoding = base64_encode_body(body)
-  end
-  return body_entity, body_transfer_encoding
 end
 
 -- Prepare message
@@ -271,8 +163,8 @@ function _M.prepare_message(config)
   if ngx.var.moesif_req_body == nil or config:get("disable_capture_request_body") or (request_content_length ~= nil and tonumber(request_content_length) > config:get("max_body_size_limit")) then
     request_body_entity = nil
   else
-    local request_body_masks = mask_body_fields(split(config:get("request_body_masks"), ","), split(config:get("request_masks"), ","))
-    request_body_entity, req_body_transfer_encoding = parse_body(request_headers, ngx.var.moesif_req_body, request_body_masks, config)
+    local request_body_masks = ser_helper.mask_body_fields(split(config:get("request_body_masks"), ","), split(config:get("request_masks"), ","))
+    request_body_entity, req_body_transfer_encoding = ser_helper.parse_body(request_headers, ngx.var.moesif_req_body, request_body_masks, config)
   end
 
   -- Response body
@@ -280,8 +172,8 @@ function _M.prepare_message(config)
   if ngx.var.moesif_res_body == nil or config:get("disable_capture_response_body") or (response_content_length ~= nil and tonumber(response_content_length) > config:get("max_body_size_limit")) then
     response_body_entity = nil
   else
-    local response_body_masks = mask_body_fields(split(config:get("response_body_masks"), ","), split(config:get("response_masks"), ","))
-    response_body_entity, rsp_body_transfer_encoding = parse_body(response_headers, ngx.var.moesif_res_body, response_body_masks, config)
+    local response_body_masks = ser_helper.mask_body_fields(split(config:get("response_body_masks"), ","), split(config:get("response_masks"), ","))
+    response_body_entity, rsp_body_transfer_encoding = ser_helper.parse_body(response_headers, ngx.var.moesif_res_body, response_body_masks, config)
   end
 
   -- Headers
@@ -290,12 +182,12 @@ function _M.prepare_message(config)
   
   -- Mask request headers
   if next(request_header_masks) ~= nil then
-    request_headers = mask_headers(ngx.req.get_headers(), request_header_masks)
+    request_headers = ser_helper.mask_headers(ngx.req.get_headers(), request_header_masks)
   end
 
   -- Mask response headers
   if next(response_header_masks) ~= nil then
-    response_headers = mask_headers(ngx.resp.get_headers(), response_header_masks)
+    response_headers = ser_helper.mask_headers(ngx.resp.get_headers(), response_header_masks)
   end
 
   -- Get session token
